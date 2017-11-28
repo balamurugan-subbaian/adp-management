aa2d0920d0a6ae37065c124b19e856866812f80c
balamurugan-subbaian <balamurugan.s@highpeaksw.com>
2017-11-28 13:29:06 +0530

    styles for component

--- src/components/SayHello/index.js
+++ src/components/SayHello/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './SayHello.scss';
 
 const SayHello = (props) => (
     <div>
-        <h3>Hello {props.name}...!</h3>
+        <h3 className="heading">Hello {props.name}...!</h3>
         <p>Welcome to adp management</p>
     </div>
 )
