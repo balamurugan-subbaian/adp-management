3b826f9ee983a00c8b6786b22f86bd50f2081be4
balamurugan-subbaian <balamurugan.s@highpeaksw.com>
2017-11-28 16:25:42 +0530

    snapshots to json

--- src/components/SayHello/SayHello.test.js
+++ src/components/SayHello/SayHello.test.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {shallow} from 'enzyme';
+import toJSON from 'enzyme-to-json';
 import SayHello from './index';
 
 describe('<SayHello />', () => {
     test('should render with one wrapper div', () => {
         const wrapper = shallow(<SayHello name="testName"/>);
         expect(wrapper.find('div')).toHaveLength(1);
-        expect(wrapper).toMatchSnapshot();
+        expect(toJSON(wrapper)).toMatchSnapshot();
     })
 })
